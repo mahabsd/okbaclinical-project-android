4c5c60a273bf8cb050f1d850f0fb40e30dcbf619
nidhal1508 <nidhal1508@gmail.com>
2021-01-15 19:41:18 +0100

    first commit

---
+++ src/app/pages/errors/error/error.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppSettings } from '../../../app.settings';
+import { Settings } from '../../../app.settings.model';
+
+@Component({
+  selector: 'app-error',
+  templateUrl: './error.component.html'
+})
+export class ErrorComponent {
+  public settings: Settings;
+  constructor(public appSettings:AppSettings, public router:Router) {
+    this.settings = this.appSettings.settings; 
+  }
+
+  goHome(): void {
+    this.router.navigate(['/']);
+  }
+
+  ngAfterViewInit(){
+    setTimeout(() => {
+      this.settings.loadingSpinner = false; 
+    });  
+  } 
+
+}
