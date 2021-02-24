4c7a4dc52cf1d3951e963ef4cabfce2e08dc2127
nidhal1508 <nidhal1508@gmail.com>
2021-02-24 11:51:15 +0100

    update

--- src/app/pages/dashboard/tiles/tiles.component.ts
+++ src/app/pages/dashboard/tiles/tiles.component.ts
@@ -3,6 +3,8 @@ import { ActionnairesService } from 'src/app/services/actionnaires.service';
 import { DoctorsService } from 'src/app/services/doctors.service';
 import { PatientsService } from 'src/app/services/patients.service';
 import { UsersService } from 'src/app/services/users.service';
+import { MaintenancesService } from 'src/app/services/maintenance.service';
+import { CongeService } from 'src/app/services/conge.service';
 import jwt_decode from "../../../../../node_modules/jwt-decode";
 
 @Component({
@@ -15,13 +17,18 @@ export class TilesComponent implements OnInit {
   public lengthAct;
   public lengthUsers;
   public lengthPatients;
+ 
+  public lengthMaints;
+
+  public lengthconges;
   public token = localStorage.getItem('token');
   public decoded = JSON.parse(JSON.stringify(jwt_decode(this.token)));
   public userId = this.decoded._id;
 
   constructor(private actionnairesService: ActionnairesService,
     private doctorsService: DoctorsService,
-    private usersService: UsersService, private patientsService : PatientsService) {
+    private usersService: UsersService, private patientsService : PatientsService,
+    private maintenancesService : MaintenancesService,private congeservice : CongeService) {
 
     this.actionnairesService.getAllActionnaires().subscribe((res: []) => {
       this.lengthAct = res.length
@@ -36,6 +43,19 @@ export class TilesComponent implements OnInit {
     this.patientsService.getAllPatients().subscribe((res: []) =>
     this.lengthPatients = res.length
   );
+  this.maintenancesService.getAllMaintenances().subscribe((res: [any]) =>{
+    
+    
+    this.lengthMaints = res.filter(re=> re.statut="validated").length;
+  }
+ 
+   
+);
+this.congeservice.getAllconges().subscribe((res: [any]) =>{
+ 
+  this.lengthconges = res.filter(re=> re.statut="validated").length;
+}
+);
   }
 
   ngOnInit() {
--- src/app/pages/form-controls/SmsSend/sms-send.component.ts
+++ src/app/pages/form-controls/SmsSend/sms-send.component.ts
@@ -43,11 +43,25 @@ export class SmsSendComponent implements OnInit {
     var decoded = jwt_decode(token);
     this.formSms.patchValue({
       userOwner: JSON.parse(JSON.stringify(decoded))._id,
+      smsOwner: JSON.parse(JSON.stringify(decoded))._id,
       status: "envoyé"
+      
+    });
+    this.smsService.addSms(this.formSms.value).subscribe(sms => {  
+      console.log("POST call successful value returned in body", sms);
+      let message = "sms added successfully";
+      ///action va etre changé
+      let action = "close"
+      this.snackBar.open(message, action, {
+        duration: 2000,
+      });
+      this.ngOnInit()
+    },
+    () => {
+      console.log("The POST observable is now completed.");
 
     });
-    this.smsService.addSms(this.formSms.value).subscribe(sms => {
-    });
+    
     this.smsService.SendSms(this.formSms.value.contacts.type, this.formSms.value.contacts.phone, this.formSms.value.contacts.message).subscribe(sms => {
       let message = "Sms added successfully";
       let action = "close"
