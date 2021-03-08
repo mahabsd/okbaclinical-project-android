cc02656d2fe97bfe85db3e6c581e169ec9f1d74a
Mahabsd <Mahabsd@gmail.com>
2021-03-08 17:38:00 +0100

    change app name

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/okba-clinical'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+res.sendFile(path.join(__dirname+'/dist/okba-clinical/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
