fcfb59921dc52ddcd4a508cc1e73fe051e87e08e
Mahabsd <Mahabsd@gmail.com>
2021-03-08 17:30:29 +0100

    fix server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/ProjetClinique-Frontend'));
+app.use(express.static(__dirname + '/dist/okba-clinical'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/ProjetClinique-Frontend/src/index.html'));
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
