35dc458afbf31f91bbb7a22c05f043851ad21933
Mahabsd <Mahabsd@gmail.com>
2021-03-10 16:15:19 +0100

    change env prod to https://okbaclinical-backend.herokuapp.com/

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  basUrl :  "https://okbaclinical.herokuapp.com/api/",
-  socketBaseUrl : "https://okbaclinical.herokuapp.com"
+  basUrl :  "https://okbaclinical-backend.herokuapp.com/api/",
+  socketBaseUrl : "https://okbaclinical-backend.herokuapp.com/"
 };
