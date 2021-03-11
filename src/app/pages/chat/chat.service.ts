e61afab9a419788e8aed51eab108944140a0a593
Mahabsd <Mahabsd@gmail.com>
2021-03-11 11:08:57 +0100

    fix path

--- src/app/pages/chat/chat.service.ts
+++ src/app/pages/chat/chat.service.ts
@@ -11,7 +11,7 @@ import { environment } from 'src/environments/environment.prod';
 
 @Injectable()
 export class ChatService {
-  private url = environment.socketBaseUrl;
+  private url = environment.basUrl;
   socket: any;
 
   constructor(private httpClient: HttpClient) {
