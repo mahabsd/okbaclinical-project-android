70639fb3c4e688c54ec301f63c1d8c7bf459e3e5
Mahabsd <Mahabsd@gmail.com>
2021-03-11 18:13:54 +0100

    change port

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   basUrl :  "https://okbaclinical-backend.herokuapp.com/api/",
-  socketBaseUrl : "https://okbaclinical-backend.herokuapp.com:8080"
+  socketBaseUrl : "https://okbaclinical-backend.herokuapp.com:8001"
 };
